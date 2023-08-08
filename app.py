550d20eeb271bb9a910244acd3fe18da322c7373
adilson-tavares <tavares.adilson.c@gmail.com>
2023-08-07 22:19:49 -0400

    feat: Adiciona route login

--- app.py
+++ app.py
@@ -11,9 +11,22 @@
 # if __name__ == "__main__":
 #     app.run()
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__,  static_folder="static")
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 @app.route("/")
 def hello_world():
     return "<p>This is a Hello World my python</p>"
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    if request.method == "POST":
+        email = request.form.get('email')
+        senha = request.form.get('password')
+        print(email, senha)
+        return "OK POST"
+    return "OK GET"
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
